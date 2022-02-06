6bbba85b4560e6c64d7782359dc69fed55ebafd5
YuriiKnysh <uriy.knish@gmail.com>
2022-02-06 15:38:52 +0400

    Changes

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    public int Index { get; set; }
+    private string _setDialogue = "";
+    public string SetDialogue
+    {
+        get => _setDialogue;
+        set
+        {
+            _setDialogue = value;
+            LangDialogue(Index);
+        }
+    }
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void LangDialogue(int index)
+    {
+        dialogue.sentences[index] = _setDialogue;
     }
 }
---
+++ Assets/loc.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+
+public class loc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SystemLanguage.English;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
